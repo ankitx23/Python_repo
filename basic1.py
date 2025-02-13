f8f1d79b7a1970b5120fabb6de32350bf2796165
Ankit <ankitxydv@gmail.com>
2025-02-13 20:55:46 +0000

    basic

---
+++ basic1.py
@@ -0,0 +1,21 @@
+#example of var, int, and arithmetic operations
+
+
+a= 5
+b=6
+c = a+b
+d = b-a
+l= a**b #exponential
+print (l) 
+print (c)
+print (d)
+ 
+e=28.48
+f=4
+g = e/f 
+j= e%f #in this case it will return the e cuz it is not divisble by 4 (modulus)
+print (g)
+print (j)
+
+
+print (type (g))
