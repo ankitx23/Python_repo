38c0f01c330c710dffed70551ca6566f99cf8cc7
Ankit <ankitxydv@gmail.com>
2025-02-17 03:06:19 +0000

    datatype & string

---
+++ datatype2.py
@@ -0,0 +1,45 @@
+#strings
+
+
+#a = ("ankit")
+#print (a)
+
+#print ("I'mabsi")
+# print ('I'mabsi') you cant excuse this line, so use backslash for that
+
+#print ('I\'m sabshi.') #use back slash 
+
+message = "ankit is good"
+#print (message[4]) #starts with zero
+
+a = len(message) # to check the length of the program
+#print (a)
+
+#using count. to check the alphabets available in the string
+
+e=message.count("an")
+#print (e)
+
+#use upper and lower in case of capital and small alphabets respectively
+
+string = "He is a good man. He loves momos & chole bhature"
+
+f = string.count("momos")
+h= string.lower().count("o")
+i= string.upper().count("H")
+
+print (f, h, i)
+
+
+#addign number in a string
+
+string2 = 25
+#add= string2 + "5" # you can't perform this action cuz you cant add string into int
+add2 = string2 + 5 #valid action 
+#print (add)
+print (add2)
+
+
+
+
+
