3d45a6173106dfed8dfc6e259a4b03275c6782d9
Ankit <ankitxydv@gmail.com>
2025-02-17 03:31:37 +0000

    list and its functions

---
+++ list.py
@@ -0,0 +1,37 @@
+#assign list to a list, it should be always in square bracket
+
+list = [1,2,3,4,7,9,0]
+print (list)
+print (type(list))
+
+#function to add the numbers in list
+
+a= sum(list)
+#print (a)
+
+# python is flexible with adding any type of item in the list like you can add int, float, string , char
+
+b =[3,4,6,7,"d", 4.9, "adsjkhg"]
+#print (b)
+
+#function to update item in a given list by using indexing using above list
+
+print (b[0])
+                #modify the list
+
+b[0]=78
+
+print (b)
+
+#print the average/mean of a llist
+
+c= [34,45,56,67,78,89,90,1,12,23]
+
+e= len(c)
+d= sum(c)/e
+
+print (d)
+
+
+
+ 
