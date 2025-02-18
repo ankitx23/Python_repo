66cba52ce9236a9371b79e8a3fea712e44b4a683
Ankit <ankitxydv@gmail.com>
2025-02-18 04:44:26 +0000

    sets

---
+++ sets.py
@@ -0,0 +1,24 @@
+#unordered collection of unique elements
+#no duplicates
+# sets are mutable
+
+#create a non empty set
+emptyset  = set()
+print (type(emptyset))
+
+# create a non empty set within curly braces
+
+set = {1,2,3,4,'sdf'}
+print (type(set))
+
+print(set)
+
+#mutability 
+set.add(7)
+set.add('goa')
+
+set.remove(2)
+print ((set))
+
+
+
