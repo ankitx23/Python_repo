5cb35d726c572dfd453a7e4849a87fbf1af7f892
Ankit <ankitxydv@gmail.com>
2025-02-18 22:19:34 +0000

    Slicing DIctionary

--- index.py
+++ index.py
@@ -1,2 +1,2 @@
-a = {"Hello, World"}
-print {a[4], a[8]}
+a = ("Hello, World")
+print (a[4], a[8])
---
+++ slicing.py
@@ -0,0 +1,42 @@
+#Normal slicing #it starts with zero from left side and from right to left is starts from -1
+
+string = "ANKIT, COMPUTER SCIENCE"
+#print (len(string))
+#print (string[0:6])
+#print (string[7:])
+#print (string[:-1])
+
+
+#Slicing with STEP - in this we use step to skip in between string
+#print (string[0:23:2]) # this will skip every 3rd. lettter
+
+
+# reversing string in python using slicing
+#print (string [::-1])
+
+#pulling CNEI from string
+#print (string[-6:-2])
+
+#------TUPLE-------#
+
+tuple = ("Computer",1,2,3,5,6,7)
+#print (tuple[0])
+#print (tuple[0:7])
+#print (tuple[0:7:2])
+
+#---------LIST-------#
+
+list=[1,2,3,4,55,6,7,8,97,4]
+#print (list[0::2])
+
+#----------Dictionaries---------# IT doesn t support sliciing but y0ou can use subset to take keys out of Dictionary
+
+dict = {"Name": "Ankit", "Class" : "Computer Science", "Subject" : "Programming"}
+#print (dict[0:1])
+
+
+
+
+
+
+
