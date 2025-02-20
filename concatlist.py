c1f79ead5b847c709d597b8af16404524ebb6a95
Ankit <ankitxydv@gmail.com>
2025-02-20 02:42:51 +0000

    concat

---
+++ concatlist.py
@@ -0,0 +1,22 @@
+#concat is adding two things
+list1= [1,2,3,4,5,6]
+list2= [3,4,5,56,6,8]
+
+result = list1+list2 #this is concatination 
+print (result)
+
+#repeating
+new = list1*3
+print (new)
+
+
+#concat in string
+string1 = "ankit"
+string2 = "yadav"
+
+add = string1 + " " + string2 + ","
+print (add)
+
+#repeating 
+new2 = add * 3
+print (new2)
