1195824fb32d1dc9e1be0e4c18dc0b089b081e80
Ankit <ankitxydv@gmail.com>
2025-02-24 21:14:21 +0000

    input

---
+++ takinginput.py
@@ -0,0 +1,15 @@
+#input is taken as a string
+
+a = input ("Enter 1st number :")
+b=  input ("Enter 2nd number :")
+
+c= a+b
+print (c) #1050
+
+# addition of two numbers (Need to convert the string into int first)
+
+a = int (input ("Enter 1st number :"))
+b=  int (input ("Enter 2nd number :"))
+c= a+b
+print (c)
+
